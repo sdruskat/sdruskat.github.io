352cc6cf8321cf31050e9e89d8576ffa95c38e17
Stephan Druskat <mail@sdruskat.net>
2018-01-02 16:15:16 +0100

    Fix build

--- build-jekyll.sh
+++ build-jekyll.sh
@@ -19,6 +19,7 @@ git clone https://${GITHUB_TOKEN}@github.com/sdruskat/sdruskat.github.io.git --b
 # build with Jekyll into "_site"
 gem install jekyll-scholar
 gem install unicode
+gem install active_support
 bundle exec jekyll build
 
 # push
