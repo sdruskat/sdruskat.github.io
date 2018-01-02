ade14139dbafc7970b33af6e6f0725a06cd2fd0c
Travis CI <travis-ci@sdruskat.net>
2018-01-02 19:26:41 +0000

    Travis #13

--- build-jekyll.sh
+++ build-jekyll.sh
@@ -18,7 +18,7 @@ git clone https://${GITHUB_TOKEN}@github.com/sdruskat/sdruskat.github.io.git --b
 
 # build with Jekyll into "_site"
 gem install jekyll-scholar
-bundle exec jekyll build
+jekyll build
 
 # push
 cd _site
